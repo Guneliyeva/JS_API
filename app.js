6f0092e06d3984fc35f9d07da1acbbcccb491936
guneliyeva <guneliyeva0214@gmail.com>
2024-04-04 19:35:15 +0400

    task modified

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const apiKey= "84a30b0cfe4896d31b35025b617bc6dc";
 console.log(`Film name: `);
-fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1")
+fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${apiKey}`)
   .then((res) => res.json())
   .then((data) => {
     data.results.map((movie) => {
       console.log(`${movie.title} || ${movie.original_title} || IMDB: ${Math.round(movie.vote_average)} `);
     });
   });
+
+
